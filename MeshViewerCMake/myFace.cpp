8048eacb1dbe1342ee3b79d80847722190a338f6
deltahack94 <paul.mallard@edu.esiee.fr>
2025-04-04 11:58:37 +0200

    first commit - readFile OBJ

---
+++ MeshViewerCMake/myFace.cpp
@@ -0,0 +1,33 @@
+#include "myFace.h"
+#include "myVector3D.h"
+#include "myHalfedge.h"
+#include "myVertex.h"
+#include <GL/glew.h>
+
+myFace::myFace(void)
+{
+	adjacent_halfedge = NULL;
+	normal = new myVector3D(1.0, 1.0, 1.0);
+}
+
+myFace::~myFace(void)
+{
+	if (normal) delete normal;
+}
+
+void myFace::computeNormal()
+{
+	myHalfedge *h = adjacent_halfedge;
+	myVector3D v1, v2;
+	v1.dX = h->source->point->X - h->next->source->point->X;
+	v1.dY = h->source->point->Y - h->next->source->point->Y;
+	v1.dZ = h->source->point->Z - h->next->source->point->Z;
+
+	h = h->next;
+	v2.dX = h->source->point->X - h->next->source->point->X;
+	v2.dY = h->source->point->Y - h->next->source->point->Y;
+	v2.dZ = h->source->point->Z - h->next->source->point->Z;
+
+	normal->crossproduct(v1, v2);
+	normal->normalize();
+}
---
+++ MeshViewerCMake/myHalfedge.cpp
@@ -0,0 +1,20 @@
+#include "myHalfedge.h"
+
+myHalfedge::myHalfedge(void)
+{
+	source = NULL; 
+	adjacent_face = NULL; 
+	next = NULL;  
+	prev = NULL;  
+	twin = NULL;  
+}
+
+void myHalfedge::copy(myHalfedge *ie)
+{
+/**** TODO ****/
+}
+
+myHalfedge::~myHalfedge(void)
+{
+	
+}
